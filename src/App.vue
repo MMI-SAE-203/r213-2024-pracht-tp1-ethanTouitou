f4cb8617d2d61cd3e23718ac309d244ee8374487
ethanTouitou <150148202+ethanTouitou@users.noreply.github.com>
2024-04-12 09:17:12 +0200

    ajout d'un transition

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { onErrorCaptured } from 'vue'
+import { onErrorCaptured, ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
 
 onErrorCaptured((err, instance, info) => {
   console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
   return true
 })
+
 const menuIsOpen = ref(false)
 </script>
 
@@ -19,14 +20,21 @@ const menuIsOpen = ref(false)
     >
       menu
     </button>
-    <!-- nav#mainNav>ul>li*3>a[href="#"]{item $} -->
-    <nav id="mainNav" v-show="menuIsOpen">
-      <ul>
-        <li><a href="#">item 1</a></li>
-        <li><a href="#">item 2</a></li>
-        <li><a href="#">item 3</a></li>
-      </ul>
-    </nav>
+    <Transition
+      class="transition-transform duration-1000"
+      enter-from-class="-translate-x-full"
+      enter-to-class="translate-x-0"
+      leave-active-class="-translate-x-full"
+    >
+      <!-- nav#mainNav>ul>li*3>a[href="#"]{item $} -->
+      <nav id="mainNav" v-show="menuIsOpen">
+        <ul>
+          <li><a href="#">item 1</a></li>
+          <li><a href="#">item 2</a></li>
+          <li><a href="#">item 3</a></li>
+        </ul>
+      </nav>
+    </Transition>
     <nav>
       <ul>
         <li>
